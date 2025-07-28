521a9c4313d934b157429a53ec0af328d58875d3
Abhishek <2203031@ritindia.edu>
2025-07-28 16:38:04 +0530

    Updated Addition with new line

---
+++ Addition.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+	int a, b,c;
+	printf("Enter the 1st Number : ");
+	scanf("%d",&a);
+	printf("Enter the 2nd Number : ");
+	scanf("%d",&b);
+	c=a+b;
+	printf("Addition of 2 Numbers is %d",c);
+}
+
